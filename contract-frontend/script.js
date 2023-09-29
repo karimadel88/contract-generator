2472c44bcc3ce4be0b37a12d5f573cc014afee10
karimadel88 <ka565872@gmail.com>
2023-09-29 18:20:52 +0200

    .

--- contract-frontend/script.js
+++ contract-frontend/script.js
@@ -2,9 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("counteractForm");
   const resultDiv = document.getElementById("resultDiv");
   const submitButton = document.getElementById("submitButton");
+  const downloadButton = document.getElementById("downloadButton");
   const spinner = document.getElementById("spinner");
   const successModal = document.getElementById("success-modal");
   const closeModalButton = document.getElementById("close-modal");
+  let responseBlob = null;
+
   submitButton.addEventListener("click", function () {
     // Show the spinner while waiting for the response
     spinner.style.display = "block";
@@ -40,16 +43,14 @@ document.addEventListener("DOMContentLoaded", function () {
         return response.blob();
       }) // Get the response as a blob
       .then(blob => {
+        responseBlob = blob;
         // Create a blob URL for the response
         const url = window.URL.createObjectURL(blob);
 
-        // Create a link to trigger the download
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = "file.html";
+        window.open(url, "_blank");
 
-        // Trigger the download
-        a.click();
+        // Show the download button
+        downloadButton.style.display = "block";
 
         // Clean up the blob URL
         window.URL.revokeObjectURL(url);
@@ -66,4 +67,29 @@ document.addEventListener("DOMContentLoaded", function () {
         resultDiv.innerHTML = "Error downloading file.";
       });
   });
+
+  // Add click event listener for the download button
+  downloadButton.addEventListener("click", function () {
+    if (responseBlob) {
+      // Create a blob URL for the response blob
+      const url = window.URL.createObjectURL(responseBlob);
+
+      // Create a link to trigger the download
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "file.html";
+
+      // Trigger the download
+      a.click();
+
+      // Clean up the blob URL
+      window.URL.revokeObjectURL(url);
+    }
+  });
+});
+
+const rerenderButton = document.getElementById("rerenderButton");
+rerenderButton.addEventListener("click", function () {
+  // Reload the page
+  location.reload();
 });
